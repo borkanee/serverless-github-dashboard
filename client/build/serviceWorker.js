6b55713e83033fa01799941ef0f82a91d85e5a06
borkanee <grubesic.boris@gmail.com>
2019-03-19 18:58:12 +0100

    Removing all files in .gitignore

--- client/build/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-console.log('Service worker loaded')
-
-self.addEventListener('push', event => {
-  const data = event.data.json()
-  self.registration.showNotification(data.title, {
-    body: data.body
-  })
-})
